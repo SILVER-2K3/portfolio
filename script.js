93e639d867bca851d43636e4088c25baee7c553f
SILVER <114167761+SILVER-2K3@users.noreply.github.com>
2025-12-16 02:38:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -37,3 +37,22 @@ document.querySelectorAll('a').forEach(links =>{
     }
 
 });
+
+
+
+// document.querySelector("#form-submit")
+// .addEventListener("click", function (event) {
+//   event.preventDefault();
+
+//   const message = document.querySelector(
+//     'textarea[name="message"]'
+//   ).value; // Get message content
+//   const email = "silver2k3@duck.com";
+//   const subject = "Inquiry from Portfolio Website";
+//   const body = encodeURIComponent(message);
+
+//   const mailtoLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`;
+
+//   // Open Gmail
+//   window.open(mailtoLink, "_blank");
+// });
